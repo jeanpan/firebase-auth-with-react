2aadabddaa683b45fc06febf05eaaa93988a4ee7
jeanpan <beats.1213@gmail.com>
2016-04-05 22:40:28 -0400

    wrap firebase auth

---
+++ app/utils/firebaseAuth.js
@@ -0,0 +1,26 @@
+'use strict';
+
+var firebaseAuth = (function() {
+  var ref = new Firebase("https://clapperboard.firebaseio.com/");
+
+  function checkLoginState(callback) {
+    ref.onAuth(callback);
+  }
+
+  function logout(callback) {
+    ref.unauth(callback);
+  }
+
+  function login(data, callback) {
+    ref.authWithPassword(data, callback);
+  }
+
+  return {
+    checkLoginState: checkLoginState,
+    logout: logout,
+    login: login
+  }
+
+})();
+
+module.exports = firebaseAuth;
